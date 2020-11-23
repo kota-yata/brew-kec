b10ea92e5adc19f352ec0f9ddc8b3cecda446f63
Kota Yatagai <kota@yatagai.com>
2020-11-23 21:19:42 +0900

    afwgr

--- kec.rb
+++ kec.rb
@@ -1,11 +1,14 @@
 class Kec < Formula
   desc "Image format extension converter. Only for jpg/png/gif image. Poor"
   homepage "https://github.com/kota-yata/kec"
-  url "https://github.com/kota-yata/kec/releases/download/1.0.1/kec"
-  sha256 "f1962237ad5d67f324b0b5d8ba2ef873fbac707225dda9fc8726fcf398f5eed1"
+  url "https://github.com/kota-yata/kec/archive/1.0.1.tar.gz"
+  sha256 "194bd97c5357216cce3a79c80f6b0912309e7ba752d94e6db5ef91ebe7c6d50d"
   license "BSD-2-Clause"
 
   def install
-    bin.install "bin/kec"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
   end
 end
