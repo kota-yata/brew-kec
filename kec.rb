f5055fb5f69e6a45ed4cba036f2210c042eab582
Kota Yatagai <kota@yatagai.com>
2020-11-23 19:21:30 +0900

    Change license

--- kec.rb
+++ kec.rb
@@ -3,7 +3,7 @@ class Kec < Formula
   homepage "https://github.com/kota-yata/kec"
   url "https://github.com/kota-yata/kec/archive/1.0.0.tar.gz"
   sha256 "9a569bae71ff0ce6a9fdc6e7e68a878480ed023997c5bd8ab3e54b5a22c46022"
-  license "MIT"
+  license "BSD-2-Clause"
 
   depends_on "go" => :build
 
