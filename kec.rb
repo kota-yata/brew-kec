52501a7f54f423cd8b5256df3d83075f79c6aff2
kota-yata <kota@yatagai.com>
2020-11-30 22:07:44 +0900

    v1.0.3

--- kec.rb
+++ kec.rb
@@ -1,8 +1,8 @@
 class Kec < Formula
   desc "Image format extension converter. Only for jpg/png/gif image. Poor"
   homepage "https://github.com/kota-yata/kec"
-  url "https://github.com/kota-yata/kec/archive/1.0.1.tar.gz"
-  sha256 "194bd97c5357216cce3a79c80f6b0912309e7ba752d94e6db5ef91ebe7c6d50d"
+  url "https://github.com/kota-yata/kec/releases/download/v1.0.3/kec_1.0.3_Darwin_x86_64.tar.gz"
+  sha256 "25ab304e1f3e94242ac9c5c9b321a3d8473c125c15c80e7a01931f174319202a"
   license "BSD-2-Clause"
 
   def install
