45901a46203d97538956504b8d350a2662562ca9
kota-yata <kota@yatagai.com>
2021-01-05 02:12:39 +0900

    publish 1.1.1

---
+++ README.md
@@ -0,0 +1,7 @@
+# for publishing kec to brew
+
+Download https://github.com/kota-yata/kec/releases/download/{tagname}/kec_{tagname}_Darwin_x86_64.tar.gz
+
+```
+shasum -a 256 
+```
--- kec.rb
+++ kec.rb
@@ -1,8 +1,8 @@
 class Kec < Formula
-  desc "Image format extension converter. Only for jpg/png/gif image. Poor"
+  desc "Image format extension convesion and compressing module. Only for jpg/png/gif image. Poor"
   homepage "https://github.com/kota-yata/kec"
-  url "https://github.com/kota-yata/kec/releases/download/v1.0.3/kec_1.0.3_Darwin_x86_64.tar.gz"
-  sha256 "25ab304e1f3e94242ac9c5c9b321a3d8473c125c15c80e7a01931f174319202a"
+  url "https://github.com/kota-yata/kec/releases/download/v1.1.1/kec_1.1.1_Darwin_x86_64.tar.gz"
+  sha256 "7e662c7aaf83da31db18a65e21fc9770e3814f652de2d5644be323cd795fd345"
   license "BSD-2-Clause"
 
   def install
